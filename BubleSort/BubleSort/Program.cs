2ba8bbc87a1c083eb967477eb03655ef38d01e35
Salih Tok <salih.tok.661@gmail.com>
2025-03-02 17:44:41 +0300

    K selection ve buble sort kodu eklendi

---
+++ BubleSort/BubleSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] sizes = { 1000, 5000, 10000, 20000 };
+        Random rand = new Random();
+
+        foreach (int size in sizes)
+        {
+            int[] array = Enumerable.Range(1, size).OrderBy(x => rand.Next()).ToArray();
+            int[] arrayCopy = (int[])array.Clone();
+
+            Stopwatch sw = Stopwatch.StartNew();
+            BubbleSort(arrayCopy);
+            sw.Stop();
+
+            Console.WriteLine($"Boyut: {size}, Bubble Sort Süresi: {sw.ElapsedMilliseconds} ms");
+        }
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        bool swapped;
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+}
---
+++ KSelectionProblem/KSelectionProblem/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] sizes = { 1000, 5000, 10000, 50000, 100000 };
+        int k = 10; // k. en küçük eleman
+        Random rand = new Random();
+
+        foreach (int size in sizes)
+        {
+            int[] array = Enumerable.Range(1, size).OrderBy(x => rand.Next()).ToArray();
+
+            int[] array1 = (int[])array.Clone();
+            int[] array2 = (int[])array.Clone();
+
+            Stopwatch sw = Stopwatch.StartNew();
+            int result1 = FindKthSmallest_Sort(array1, k);
+            sw.Stop();
+            Console.WriteLine($"Boyut: {size}, Sıralama Yöntemi: {sw.ElapsedMilliseconds} ms, Sonuç: {result1}");
+
+            sw.Restart();
+            int result2 = FindKthSmallest_PartialSort(array2, k);
+            sw.Stop();
+            Console.WriteLine($"Boyut: {size}, Kısmi Sıralama: {sw.ElapsedMilliseconds} ms, Sonuç: {result2}\n");
+        }
+    }
+
+    static int FindKthSmallest_Sort(int[] arr, int k)
+    {
+        Array.Sort(arr);
+        return arr[k - 1];
+    }
+
+    static int FindKthSmallest_PartialSort(int[] arr, int k)
+    {
+        Array.Sort(arr, 0, k);
+        for (int i = k; i < arr.Length; i++)
+        {
+            if (arr[i] < arr[k - 1])
+            {
+                int j = k - 1;
+                while (j > 0 && arr[j - 1] > arr[i])
+                {
+                    arr[j] = arr[j - 1];
+                    j--;
+                }
+                arr[j] = arr[i];
+            }
+        }
+        return arr[k - 1];
+    }
+}
